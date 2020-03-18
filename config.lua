52414c34dcde3def55cfebffc4f092e6d9252a74
istoking <58592678+istoking@users.noreply.github.com>
2020-03-18 13:53:06 +1300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+Config.DrawDistance = 100
+Config.Size         = {x = 1.5, y = 1.5, z = 1.0}
+Config.Color        = {r = 139, g = 16, b = 20}
+Config.Type         = 27
+
+Config.Logouts = {
+	vector3(-211.19, -1032.94, 30.14),
+	vector3(115.45, -1723.23, 30.11),
+	vector3(450.71, -650.83, 28.45),
+	vector3(310.94, -579.41, 43.28),
+	vector3(-845.59, -123.76, 28.18)
+}
